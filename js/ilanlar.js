d77b9f4075bf7a8086afc0bf9ca53671f3a55152
ademcolak68-debug <ademcolak68@gmail.com>
2026-07-10 23:34:56 +0300

    İlan güncellendi (Orijinal format korunarak)

--- js/ilanlar.js
+++ js/ilanlar.js
@@ -1,1 +1,32 @@
-const ilanlarVerisi = [];
+const ilanlarVerisi = [
+    {
+        "id": 1783715695131,
+        "baslik": "deneme3",
+        "durum": "kat-karsiligi",
+        "tip": "",
+        "konutTipi": "",
+        "konutSekli": "",
+        "il": "",
+        "ilce": "",
+        "mahalle": "",
+        "fiyat": "",
+        "paraBirimi": "",
+        "aciklama": "",
+        "koor": "34.009092, 38.369623",
+        "klasorIsmi": "",
+        "resimler": [],
+        "videoUrl": "",
+        "İlan Tarihi": "",
+        "M²": "",
+        "Ada": "",
+        "Parsel": "",
+        "Oda Sayısı": "",
+        "Bina Yaşı": "",
+        "Bulunduğu Kat": "",
+        "Kat Sayısı": "",
+        "Isıtma": "",
+        "Eşyalı mı?": "",
+        "Kullanım Durumu": "",
+        "Site İçerisinde?": ""
+    }
+];
