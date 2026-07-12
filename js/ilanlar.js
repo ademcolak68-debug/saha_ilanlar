3728b33b19190968453c463ed85245f9e96b19e4
ademcolak68-debug <ademcolak68@gmail.com>
2026-07-12 22:24:24 +0300

    İlan güncellendi (Orijinal format korunarak)

--- js/ilanlar.js
+++ js/ilanlar.js
@@ -1,1 +1,35 @@
-const ilanlarVerisi = [];
+const ilanlarVerisi = [
+    {
+        "id": 1783884254695,
+        "baslik": "deneme",
+        "durum": "satilik",
+        "tip": "Konut",
+        "konutTipi": "Apartman Dairesi",
+        "konutSekli": "Daire",
+        "il": "Aksaray",
+        "ilce": "Merkez",
+        "mahalle": "Büyükbölcek",
+        "fiyat": "3.000.000",
+        "paraBirimi": "TL",
+        "aciklama": "",
+        "koor": "34.008943, 38.369710",
+        "klasorIsmi": "deneme",
+        "resimler": [
+            "images/deneme/1.jpeg",
+            "images/deneme/2.jpeg"
+        ],
+        "videoUrl": "",
+        "İlan Tarihi": "2026-07-12",
+        "M²": "130",
+        "Ada": "",
+        "Parsel": "",
+        "Oda Sayısı": "3+1",
+        "Bina Yaşı": "12",
+        "Bulunduğu Kat": "2",
+        "Kat Sayısı": "5",
+        "Isıtma": "Kombi",
+        "Eşyalı mı?": "Hayır",
+        "Kullanım Durumu": "Boş",
+        "Site İçerisinde?": "Evet"
+    }
+];
