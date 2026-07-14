3a8765cb13263e0a450d52c405dcc18948391a1c
ademcolak68-debug <ademcolak68@gmail.com>
2026-07-14 17:33:19 +0300

    İlan güncellendi (Orijinal format korunarak)

--- js/ilanlar.js
+++ js/ilanlar.js
@@ -1,38 +1,5 @@
 const ilanlarVerisi = [
     {
-        "id": 1784030752771,
-        "baslik": "Paşacık mah kiralık daire",
-        "durum": "kiralik",
-        "tip": "Konut",
-        "konutTipi": "Apartman Dairesi",
-        "konutSekli": "Daire",
-        "il": "Aksaray",
-        "ilce": "Merkez",
-        "mahalle": "Büyükbölcek",
-        "fiyat": "25.000",
-        "paraBirimi": "TL",
-        "aciklama": "",
-        "koor": "34.009021, 38.369519",
-        "klasorIsmi": "büyükböcek",
-        "resimler": [
-            "images/büyükböcek/1.jpeg",
-            "images/büyükböcek/2.jpeg"
-        ],
-        "videoUrl": "",
-        "İlan Tarihi": "2026-07-14",
-        "M²": "135",
-        "Ada": "",
-        "Parsel": "",
-        "Oda Sayısı": "",
-        "Bina Yaşı": "12",
-        "Bulunduğu Kat": "2",
-        "Kat Sayısı": "5",
-        "Isıtma": "Kombi",
-        "Eşyalı mı?": "Hayır",
-        "Kullanım Durumu": "Kiracılı",
-        "Site İçerisinde?": "Evet"
-    },
-    {
         "id": 1784039479315,
         "baslik": "Hacılar Harmanı Tapan Rezidans ta",
         "durum": "satilik",
