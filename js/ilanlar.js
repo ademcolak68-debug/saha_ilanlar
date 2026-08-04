b4684fb92c7b1874d9a00004f4161ed529535fae
ademcolak68-debug <ademcolak68@gmail.com>
2026-08-04 23:08:52 +0300

    İlan güncellendi (Orijinal format korunarak)

--- js/ilanlar.js
+++ js/ilanlar.js
@@ -132,5 +132,52 @@ const ilanlarVerisi = [
         "Eşyalı mı?": "Hayır",
         "Kullanım Durumu": "Boş",
         "Site İçerisinde?": "Hayır"
+    },
+    {
+        "id": 1785874099365,
+        "baslik": "Paşacık Mahallesinde",
+        "durum": "kiralik",
+        "tip": "Konut",
+        "konutTipi": "Apartman Dairesi",
+        "konutSekli": "Daire",
+        "il": "Aksaray",
+        "ilce": "Merkez",
+        "mahalle": "Paşacıkk",
+        "fiyat": "20.000",
+        "paraBirimi": "TL",
+        "aciklama": "Gülümse Parkı Civarında",
+        "koor": "34.018623, 38.355615",
+        "klasorIsmi": "gulumsepark",
+        "resimler": [
+            "images/gulumsepark/1.jpeg",
+            "images/gulumsepark/2.jpeg",
+            "images/gulumsepark/3.jpeg",
+            "images/gulumsepark/4.jpeg",
+            "images/gulumsepark/5.jpeg",
+            "images/gulumsepark/6.jpeg",
+            "images/gulumsepark/7.jpeg",
+            "images/gulumsepark/8.jpeg",
+            "images/gulumsepark/9.jpeg",
+            "images/gulumsepark/10.jpeg"
+        ],
+        "videoUrl": [
+            "images/gulumsepark/video_1785874116020_0.mp4"
+        ],
+        "İlan Tarihi": "2026-08-04",
+        "yetki": {
+            "durum": false,
+            "yetkiNo": "NET-YTK-1785874099366"
+        },
+        "M²": "80",
+        "Ada": "",
+        "Parsel": "",
+        "Oda Sayısı": "2+1",
+        "Bina Yaşı": "1",
+        "Bulunduğu Kat": "1",
+        "Kat Sayısı": "5",
+        "Isıtma": "Kombi",
+        "Eşyalı mı?": "Hayır",
+        "Kullanım Durumu": "Boş",
+        "Site İçerisinde?": "Hayır"
     }
 ];
