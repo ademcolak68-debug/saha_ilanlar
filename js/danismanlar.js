65c150ecb421349c073668c4f16f4387062b53ce
ademcolak68-debug <ademcolak68@gmail.com>
2026-08-12 23:46:00 +0300

    Add files via upload

---
+++ js/danismanlar.js
@@ -0,0 +1,36 @@
+const danismanlar = {
+  adem: {
+    id: "adem",
+    ad: "ADEM ÇOLAK",
+    telefon: "0532 684 28 03",
+    whatsapp: "905326842803"
+  },
+
+  muhammet: {
+    id: "muhammet",
+    ad: "MUHAMMET AKPINAR",
+    telefon: "0507 647 59 68",
+    whatsapp: "905076475968"
+  },
+  nedim: {
+    id: "nedim",
+    ad: "NEDİM DENİZ",
+    telefon: "0532 200 04 01",
+    whatsapp: "905322000401"
+  },
+  kazim: {
+    id: "kazim",
+    ad: "KAZIM KAYABAŞI",
+    telefon: "0543 717 66 68",
+    whatsapp: "905437176668"
+  },
+  ekrem: {
+    id: "ekrem",
+    ad: "EKREM DENİZ",
+    telefon: "0542 387 64 12",
+    whatsapp: "905423876412"
+  }
+};
+
+
+
